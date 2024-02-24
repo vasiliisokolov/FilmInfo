e22dbc36c2469b12173206edbbc7cf9ced59534b
Basileus <Basileus@DESKTOP-M14F8KM>
2024-02-24 18:30:37 +0300

    first good

--- FilmInfo.cpp
+++ FilmInfo.cpp
@@ -2,8 +2,37 @@
 
 int main()
 {
-	std::ifstream f("films.json");
-	json data = json::parse(f);
+	
+	std::ifstream f("films.json", std::ios::in);
+	if (f.is_open())
+	{
+		json data = json::parse(f);
 
+		SearchingOne want;
+		std::cout << "Enter actor's name:" << std::endl;
+		std::getline(std::cin, want.name);
 
+		std::cout << want.name << std::endl;
+		
+		json::iterator it = data.begin();
+		std::cout << *it;
+		
+
+		/*auto finder = data.find(want.name);
+
+		if (finder != data.end())
+		{
+			want.role = *finder;
+		}
+		else
+		{
+			std::cout << "An actor is not found!" << std::endl;
+		}
+		*/
+		f.close();
+	}
+	else
+	{
+		std::cerr << "File not found!" << std::endl;
+	}
 }
--- FilmInfo.h
+++ FilmInfo.h
@@ -1,13 +1,12 @@
 ﻿#pragma once
 #include <iostream>
 #include <fstream>
-#include "nlohmann/json.hpp"
+#include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
 struct SearchingOne
 {
 	std::string name;
-	std::string family;
 	std::string film;
-
+	std::string role;
 };
