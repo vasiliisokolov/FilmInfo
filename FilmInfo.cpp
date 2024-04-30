9fe6e7fa9200d611d094e92a7f994c93e078fb56
Basileus <Basileus@DESKTOP-M14F8KM>
2024-04-30 21:34:37 +0300

    first step

--- FilmInfo.cpp
+++ FilmInfo.cpp
@@ -10,32 +10,35 @@ int main()
 
 		SearchingOne want;
 		std::cout << "Enter actor's name:" << std::endl;
-		//std::getline(std::cin, want.name);
+		std::getline(std::cin, want.name);
 
-		//std::cout << want.name << std::endl;
+		std::cout << want.name << std::endl;
 		
 		//json::iterator it = data.begin();
 		
 		for (const auto& obj : data)
 		{
-			std::cout << obj << std::endl;
-			std::cout << obj["Starring"] << std::endl;
+			//std::cout << obj << std::endl;
+			if (obj["Starring"].contains(want.name))
+			{
+					
+				auto find_film = data.find(obj);
+				auto find_role = obj["Starring"].find(want.name);
+				want.role = *find_role;
+				std::cout << *find_role << std::endl;
+				break;
+			}
+			else
+			{
+				std::cout << "Searching!" << std::endl;
+			}
+			//std::cout << obj["Starring"] << std::endl;
 		}
 			
-				
+		
 		
 
-		/*auto finder = data.find(want.name);
-
-		if (finder != data.end())
-		{
-			want.role = *finder;
-		}
-		else
-		{
-			std::cout << "An actor is not found!" << std::endl;
-		}
-		*/
+		
 		f.close();
 	}
 	else
